e88de6abb37d6e9ed89e01e2c9cecde7e344760a
Thomas Völk <thomas@Toms-MBP.fritz.box>
2017-10-29 21:53:10 +0100

    deleted classes generated by maven archetype

--- src/main/kotlin/org/voelk/vacation/Hello.kt
+++
@@ -1,6 +0,0 @@
-package org.voelk.vacation
-
-fun main(args: Array<String>) {
-    println("Hello, World")
-}
-
--- src/test/kotlin/org/voelk/vacation/HelloTest.kt
+++
@@ -1,8 +0,0 @@
-package org.voelk.vacation
-
-import org.junit.Test
-import kotlin.test.assertEquals
-
-class HelloTest {
-
-}
