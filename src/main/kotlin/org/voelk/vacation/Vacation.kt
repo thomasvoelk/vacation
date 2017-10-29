014c744c4b4f3b9d4255dde25185484ffc792f49
Thomas Völk <thomas@Toms-MBP.fritz.box>
2017-10-29 22:05:42 +0100

    not sure why I did this. I just like it better

--- src/main/kotlin/org/voelk/vacation/Vacation.kt
+++ src/main/kotlin/org/voelk/vacation/Vacation.kt
@@ -3,18 +3,17 @@ package org.voelk.vacation
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
 
-class Vacation(start: LocalDate, end: LocalDate) {
-
-    private val durationInDays = start.until(end.plusDays(1), ChronoUnit.DAYS)
+class Vacation(private val start: LocalDate, private val end: LocalDate) {
 
     init {
         if (end.isBefore(start))
             throw IllegalArgumentException("Start must be before end")
+        val durationInDays = durationInDays()
         if (durationInDays > 1000)
             throw IllegalArgumentException("Maximum allowed period is 1000 days. Your period is $durationInDays days")
     }
 
     fun durationInDays(): Long {
-        return durationInDays
+        return ChronoUnit.DAYS.between(start, end.plusDays(1))
     }
 }
